6698fb0f9505665fd79f83bea78ecc6ebce1c954
Ilya Nemihin <nemilya@gmail.com>
2011-05-03 16:28:00 +0600

    +version

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_greenmine do
   name 'Redmine Greenmine plugin'
   author 'Ilya Nemihin'
   description 'This is a plugin for Redmine'
-  version '0.0.1'
+  version '0.0.2'
   url 'https://github.com/nemilya/redmine_greenmine'
   author_url ''
 end
