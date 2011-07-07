bd006275c6d22f864bb595892bd44d4d5cc0dc30
Ilya Nemihin <nemilya@gmail.com>
2011-07-07 18:57:12 +0600

    fix

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_greenmine do
   name 'Redmine Greenmine plugin'
   author 'Ilya Nemihin'
   description 'This is a plugin for Redmine'
-  version '0.0.6'
+  version '0.0.7'
   url 'https://github.com/nemilya/redmine_greenmine'
   author_url ''
 
@@ -30,19 +30,19 @@ require 'dispatcher'
 Dispatcher.to_prepare :redmine_greenmine do
 
   require_dependency 'issue'
-   Issue.safe_attributes "gm_responsible_id"
+  Issue.safe_attributes "gm_responsible_id"
 
-      Issue.safe_attributes 'issue_edit_start_date',
+  Issue.safe_attributes 'start_date',
       :if => lambda {|issue, user| issue.new_record? || user.allowed_to?(:issue_edit_start_date, issue.project) }
-      Issue.safe_attributes 'issue_edit_due_date',
+  Issue.safe_attributes 'due_date',
       :if => lambda {|issue, user| issue.new_record? || user.allowed_to?(:issue_edit_due_date, issue.project) }
-      Issue.safe_attributes 'issue_edit_priority',
+  Issue.safe_attributes 'priority',
       :if => lambda {|issue, user| issue.new_record? || user.allowed_to?(:issue_edit_priority, issue.project) }
-      Issue.safe_attributes 'issue_edit_assigned_to',
+  Issue.safe_attributes 'assigned_to',
       :if => lambda {|issue, user| issue.new_record? || user.allowed_to?(:issue_edit_assigned_to, issue.project) }
-      Issue.safe_attributes 'issue_edit_done_ratio',
+  Issue.safe_attributes 'done_ratio',
       :if => lambda {|issue, user| issue.new_record? || user.allowed_to?(:issue_edit_done_ratio, issue.project) }
-      Issue.safe_attributes 'issue_edit_status',
+  Issue.safe_attributes 'status',
       :if => lambda {|issue, user| issue.new_record? || user.allowed_to?(:issue_edit_status, issue.project) }
 
   Issue.send(:include, RedmineGreenmine::Patches::IssuePatch)
