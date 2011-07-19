8623273154133c4b5ebbe3f7e80ce793148ce019
Ilya Nemihin <nemilya@gmail.com>
2011-07-19 19:05:52 +0600

    permission for subtask - allow create subtasks

--- init.rb
+++ init.rb
@@ -26,7 +26,7 @@ end
 require 'issues_helper_patch'
 require 'application_helper_patch'
 require 'redmine_export_pdf_patch' # overwrite base class
-
+require 'fix_menu'
 
 require 'dispatcher'
 Dispatcher.to_prepare :redmine_greenmine do
---
+++ lib/fix_menu.rb
@@ -0,0 +1,25 @@
+module Redmine
+  module MenuManager
+    module MenuHelper
+      def allowed_node?(node, user, project)
+        # ilya
+        if node.url == { :controller => 'issues', :action => 'new' }
+          return false if !user.allowed_to?(:new_issue, @project)
+        end
+        # /ilya
+
+        if node.condition && !node.condition.call(project)
+          # Condition that doesn't pass
+          return false
+        end
+
+        if project
+          return user && user.allowed_to?(node.url, project)
+        else
+          # outside a project, all menu items allowed
+          return true
+        end
+      end
+    end
+  end
+end
