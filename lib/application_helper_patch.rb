a355c67c904639fcad62568bb9e2bcc5aab0d49f
Ilya Nemihin <nemilya@gmail.com>
2012-04-28 13:54:09 +0600

    missing helper

--- lib/application_helper_patch.rb
+++ lib/application_helper_patch.rb
@@ -8,6 +8,21 @@ module ApplicationHelperPatch
   end
 
   module InstanceMethods
+
+    def principals_options_for_select2(collection, selected=nil)
+      s = ''
+      groups = ''
+      collection.sort.each do |element|
+        selected_attribute = ' selected="selected"' if option_value_selected?(element, selected)
+        (element.is_a?(Group) ? groups : s) << %(<option value="#{element.id}"#{selected_attribute}>#{h element.name}</option>)
+      end
+      unless groups.empty?
+        s << %(<optgroup label="#{h(l(:label_group_plural))}">#{groups}</optgroup>)
+      end
+      s
+    end
+    
+    
     def link_to_issue_with_title(issue, options={})
       title = nil
       subject = nil
